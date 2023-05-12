46899b0dc0a792d18abfdf6313b07d64ef458f16
Gexi0619 <sungexi@hotmail.com>
2023-05-12 08:14:39 +0200

    v1.0.3 为popup的弹出增加一秒延迟

--- background.js
+++ background.js
@@ -2,12 +2,14 @@ chrome.runtime.onInstalled.addListener(function(details) {
   if (details.reason == "install" || details.reason == "update") {
     chrome.storage.local.get(["username", "password", "usertype"], function(data) {
       if (!data.username || !data.password || !data.usertype) {
-        chrome.windows.create({
-          url: "popup.html",
-          type: "popup",
-          width: 500,
-          height: 500
-        });
+        setTimeout(function() {
+          chrome.windows.create({
+            url: "popup.html",
+            type: "popup",
+            width: 500,
+            height: 500
+          });
+        }, 1000); // 1 秒延迟
       }
     });
   }
